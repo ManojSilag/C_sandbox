83e9afb2e9222e896ea74e82e74da3addd4ee9fa
ManojSilag <manojsilag@gmail.com>
2020-04-13 19:23:45 +0530

    char

--- section3/function.c
+++ section3/function.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 int add();
+
 int main()
 {
     int a = 2,b =5 , c;
@@ -10,9 +11,9 @@ int main()
     return 0;
 }
 
-int add( int g, int l)
+int add( int g, int b)
 {    
     int result;
-    result = g +  l;
+    result = g +  b;
     return result;
 }
