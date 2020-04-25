f47c142da71dd930d0f1f2b38a72215f4b39463e
ManojSilag <manojsilag@gmail.com>
2020-04-25 16:19:28 +0530

    structure

--- section6/structure.c
+++ section6/structure.c
@@ -11,19 +11,27 @@ int main()
     };
 
     struct date today;
-    struct date yerstday;
+    struct date yesterday;
 
 
     today.month = 9;
     today.day = 25;
     today.year = 2019;
 
-    struct date tomaroww = {4, 3, 1996};
-    yerstday = (struct date){31,31,3012};
+    struct date tomorrow = {4, 3, 1996};
+    yesterday = (struct date){31,31,3012};
 
-    printf("%i/%i/%i\n", yerstday.month, yerstday.day, yerstday.year);
-    printf("%i/%i/%i\n", tomaroww.month, tomaroww.day, tomaroww.year);
+    printf("%i/%i/%i\n", yesterday.month, yesterday.day, yesterday.year);
+    printf("%i/%i/%i\n", tomorrow.month, tomorrow.day, tomorrow.year);
     printf("%i/%i/%i\n", today.month, today.day, today.year);
+
+     struct date myDates[3] = { {12,12,1232}, {12,32,1890}};
+
+    //  struct date myDates[3] = { [2]= {1,2,3211 } };
+     
+    printf("%i/%i/%i\n", myDates[0].month, myDates[0].day, myDates[0].year);
+    printf("%i/%i/%i\n", myDates[1].month, myDates[1].day, myDates[1].year);
+    printf("%i/%i/%i\n", myDates[2].month, myDates[2].day, myDates[2].year);
 
     return 0;
 }
